ea6fa83e2207959586b590081d5a13ce98dad60c
KIGAMEKUN <reksa.prayoga1012@gmail.com>
2026-01-01 11:39:39 +0700

    final build 2

--- craco.config.js
+++ craco.config.js
@@ -1,25 +1,38 @@
 // Load configuration from environment or config file
-const path = require('path');
+const path = require("path");
+const Critters = require("critters-webpack-plugin");
 
 // Environment variable overrides
 const config = {
-  disableHotReload: process.env.DISABLE_HOT_RELOAD === 'true',
+  disableHotReload: process.env.DISABLE_HOT_RELOAD === "true",
 };
 
 module.exports = {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
     configure: (webpackConfig) => {
-      
+      const isProd = process.env.NODE_ENV === "production";
+
+      // ✅ Inline critical CSS hanya saat production build
+      if (isProd) {
+        webpackConfig.plugins.push(
+          new Critters({
+            preload: "swap",
+            pruneSource: true,
+            compress: true,
+          })
+        );
+      }
+
       // Disable hot reload completely if environment variable is set
       if (config.disableHotReload) {
         // Remove hot reload related plugins
-        webpackConfig.plugins = webpackConfig.plugins.filter(plugin => {
-          return !(plugin.constructor.name === 'HotModuleReplacementPlugin');
+        webpackConfig.plugins = webpackConfig.plugins.filter((plugin) => {
+          return !(plugin.constructor.name === "HotModuleReplacementPlugin");
         });
-        
+
         // Disable watch mode
         webpackConfig.watch = false;
         webpackConfig.watchOptions = {
@@ -30,16 +43,16 @@ module.exports = {
         webpackConfig.watchOptions = {
           ...webpackConfig.watchOptions,
           ignored: [
-            '**/node_modules/**',
-            '**/.git/**',
-            '**/build/**',
-            '**/dist/**',
-            '**/coverage/**',
-            '**/public/**',
+            "**/node_modules/**",
+            "**/.git/**",
+            "**/build/**",
+            "**/dist/**",
+            "**/coverage/**",
+            "**/public/**",
           ],
         };
       }
-      
+
       return webpackConfig;
     },
   },
